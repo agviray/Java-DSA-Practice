a101b2e32f50c727381c42425df547fd10e9ebce
Allan Viray <allang.v.dev@gmail.com>
2024-01-08 14:33:45 -0600

    Complete logic in findMissingNumber function

--- src/MissingNumber.java
+++ src/MissingNumber.java
@@ -39,10 +39,29 @@ import java.util.*;
 // None
 public class MissingNumber {
     public static int findMissingNuber (int[] nums) {
-        // ***************
-        // logic goes here
-        // ***************
-        return 0;
+        // Organize values in nums by sorting them from least to greatest.
+        Arrays.sort(nums);
+        // Initialize missingNum to be length of nums.
+        // - Plan is to iterate over nums, and compare nums[i] to a value that represents the
+        //   current index of nums[i], because the index will always increment by 1.
+        // - missingNum is initialized as nums.length in case the missing number is not equal
+        //   to index of the last number in nums.
+        int missingNum = nums.length;
+        int index = 0;
+
+        for (int num : nums) {
+            // I am relying on index as the comparing value to num in nums, because index will
+            // always only increment by 1.
+            if (num != index) {
+                // Numers in nums are sorted now, so if the num != index, then index is the missing number.
+                missingNum = index;
+                return missingNum;
+            } else {
+                // Increment as needed.
+                index++;
+            }
+        }
+
 
     }
 
